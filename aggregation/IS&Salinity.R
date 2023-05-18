f6501aa16a65535a90d2d342f6b0098a6d04d02d
Periklis_ts <ptsirostsib@gmail.com>
2023-05-18 14:31:16 +0300

    Update IS&Salinity.R
    Added code for estimating hardness and molar concentration of ions of different valence

--- aggregation/IS&Salinity.R
+++ aggregation/IS&Salinity.R
@@ -1,9 +1,11 @@
+
 MR <- list("KH2PO4" = 136.086, "NaCl" = 58.44,  "Na2HPO4" = 141.96,"Na2HPO4·7H20" = 141.96+7*18.02, "Ca(NO3)2" = 164.1 ,
            "Ca(NO3)2·4H20" = 164.1+4*18.02 ,
            "KCl" = 74.55, "MgSO4" = 120.366, "MgSO4·7H2O" = 120.366+ 7*18.02,  'NaHCO3'=84.007 , "CaCl2" =110.98 ,
            "CaCl2·2H20" =110.98+2*18.02 , "Fe(NO3)3" = 241.86,
            "Fe(NO3)3·9H20" = 241.86+9*18.02,
-           "NaH2PO4" = 119.98, "NaH2PO4·H20" = 119.98+18.02,  "MgCl2" = 95.211,  "MgCl2·67H20" = 95.211+6*18.02, "Na3C6H5O7" = 258.06, "Na2CO3" = 105.99, "NaNO3" = 85,
+           "NaH2PO4" = 119.98, "NaH2PO4·H20" = 119.98+18.02,  "MgCl2" = 95.211,  "MgCl2·67H20" = 95.211+6*18.02, "Na3C6H5O7" = 258.06,
+           "Na2CO3" = 105.99, "NaNO3" = 85,
            "Na2MgEDTA" = 358.50 ,"FeCl3" = 162.2, "FeCl3·6H20" = 162.2+6*18.02,  "H3BO3" = 61.83, "MnCl2" = 125.84,
            "MnCl2·4H20" = 125.84+4*18.02, "ZnSO4" = 161.47 , "ZnSO4·7H20" = 161.47+7*18.02,
            "CuSO4" = 159.61, "CuSO4·5H20" = 159.61+5*18.02,"Co(NO3)2" = 182.94, "Co(NO3)2·6H20" = 182.94+6*18.02,
@@ -94,7 +96,9 @@ media <- list("PBS" = list("KH2PO4" = 144,  "NaCl" = 9000, "Na2HPO4·7H20" = 795)
                                 "CaCl2" = 18, "MgSO4·7H2O" = 15, "K2HPO4" = 1.6,
                                 "FeCl3·6H20" = 0.08, "Na2EDTA" = 0.1, "H3BO3" = 0.185,
                                 "MnCl2·4H20" = 0.415, "ZnCl2" = 0.003, "CoCl2·6H20" = 0.0015, "Na2MoO4·2H20" = 0.007,
-                                "CuCl2·2H20" = 0.0001)) #mg/L
+                                "CuCl2·2H20" = 0.0001),
+              
+              "f2" = list("NaNO3" = 75, "NaH2PO4·H20" = 5, "Na2CO3")) #mg/L
 
 
 IS <- list("PBS" = 0,  "RPMI1640" = 0, "DMEM" = 0, "L-15" = 0,  "EMEM" = 0,
@@ -114,4 +118,49 @@ for(medium in names(media)){
   }
 }
 
+# Function for estimating alkalinity, hardness and ion molarity
+#if molar is TRUE, provide the molarity of each ion in mol/L, else provide ion mass in mg/L
+ion_calc <- function(Mg = 0, Ca = 0, Zn = 0, Cu = 0, Na = 0, Al = 0, K = 0, 
+                 Fe = 0, Cl = 0, NO3 = 0, SO4 = 0,  PO4 =0, CO3 = 0, HCO3 = 0, H2PO4 = 0, HPO4 = 0, BO3 =  0, 
+                 MoO4 = 0, SiO3 = 0, NH4  = 0 ,
+                 Co = 0,  C6H5O7 = 0, EDTA = 0, molar = FALSE){
+
+
+   ion_charge<- list("K" = 1,  "Na" = 1, "NH4"  = 1 , "Mg" = 2, "Ca" = 2, "Zn" = 2, "Cu" = 2, "Co" = 2,"Al" = 3,  
+                 "Fe" = 3, "Cl" = -1, "NO3" = -1, "HCO3" = -1,"H2PO4" = -1, "SO4" = -2,   "CO3" = -2,
+                  "HPO4" = -2, "MoO4" = -2, "SiO3" = -2, "BO3" =  -3,
+                   "C6H5O7" = -3, "PO4" = -3,"EDTA" = -3)
+  
+   ion_MR <- list("Mg" = 24.31, "Ca" = 40.08, "Zn" = 65.38, "Cu" = 63.55, "Na" = 22.99, "Al" = 26.98, "K" = 39.10, 
+                 "Fe" = 55.85, "Cl" = 35.45, "NO3" = 62.01, "SO4" = 96.06,  "PO4" =94.97, "CO3" = 100.09,
+                 "HCO3" = 61.02, "H2PO4" = 96.99,"HPO4" = 95.99, "BO3" =  58.81, "MoO4" = 159.95, "SiO3" = 76.08, "NH4"  = 18.04 ,
+                 "Co" = 28.01, "C6H5O7" = 498.46, "EDTA" = 292.24 )
+  
+  if(molar == FALSE){
+    
+    hardness <- Mg + Ca + Zn+ Cu+ Co #in mg/L
+    for (compound in names(ion_charge)){
+      #convert atoms/molecules of arguments from mass concentration (mg/L) to molar concentration (mol/L)
+      eval(parse(text=paste(compound,"=",compound, "/(ion_MR[[compound]]*1000)",sep="")))
+    }
+    molar_fractions <- list("+1" = K+Na+NH4, "+2" = Mg+Ca+Zn+Cu+Co , "+3"= Al+Fe,
+                            "-1" = Cl+NO3+HCO3+H2PO4, "-2" = SO4+CO3+HPO4+MoO4+SiO3, "-3" = BO3+C6H5O7+PO4+EDTA ) #in mol/L
+  }else{
+    molar_fractions <- list("+1" = K+Na+NH4, "+2" = Mg+Ca+Zn+Cu+Co , "+3"= Al+Fe,
+                            "-1" = Cl+NO3+HCO3+H2PO4, "-2" = SO4+CO3+HPO4+MoO4+SiO3, "-3" = BO3+C6H5O7+PO4+EDTA  )
+    for (compound in names(ion_charge)){
+      #convert atoms/molecules of arguments from mass concentration (mg/L) to molar concentration (mol/L)
+      eval(parse(text=paste(compound,"=",compound,"*(ion_MR[[compound]]*1000)",sep="")))
+    }
+    hardness <- Mg + Ca + Zn+ Cu+ Co
+  }
+  return(list("molar_fractions" = molar_fractions, "hardness" = hardness))
+}
+
+
+
+#Zhang et al.2008
+
+zhang_2008_lake <- ion_calc(Cl = 22.59, NO3 = 1.17, SO4 = 16.62, Ca = 47.3, Mg = 2.92,   molar = FALSE)
+zhang_2008_ww <- ion_calc(Cl = 241.1, NO3 = 11.8, SO4 = 682.9, Ca = 13.9, Mg = 3.17,   molar = FALSE)
 
